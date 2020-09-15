8e5a5e6d83d6ea426ed01aa8481207aa31517405
rokurouC <rokuroukenta@gmail.com>
2020-09-15 11:29:06 +0800

    clear config

--- src/configurations.js
+++ src/configurations.js
@@ -10,12 +10,12 @@ export const peerConnectionConfig = {
 };
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBoAgZUk5hAG5iahISmTgANO0BJ7IlaRAE",
-    authDomain: "web-rtc-rokurouc.firebaseapp.com",
-    databaseURL: "https://web-rtc-rokurouc.firebaseio.com",
-    projectId: "web-rtc-rokurouc",
-    storageBucket: "web-rtc-rokurouc.appspot.com",
-    messagingSenderId: "643814106878",
-    appId: "1:643814106878:web:3c1809e2f099919141a7af",
-    measurementId: "G-E0RGHVJKP1"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
